b0c00d02ab95ecae1c47dd02a0c0a55415f04b20
INGCapaDev <ingcapadev@gmail.com>
2024-10-01 13:04:12 -0700

    :sparkles: [FEAT](http): intro to http clients in Go

---
+++ http/1-communicating/go.mod
@@ -0,0 +1,3 @@
+module github.com/ingcapadev/ioaa
+
+go 1.23.0
---
+++ http/1-communicating/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	items, err := getItemData()
+	if err != nil {
+		log.Fatalf("error getting item data: %v", err)
+	}
+
+	// Don't edit above this line
+
+	fmt.Println(string(items))
+}
