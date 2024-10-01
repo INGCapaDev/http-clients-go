e795b9ceec4b186bd9d326b170258b777af3b00e
INGCapaDev <ingcapadev@gmail.com>
2024-10-01 13:13:57 -0700

    :sparkles: [FEAT](http): HTTP URLs in Go

---
+++ http/2-urls/go.mod
@@ -0,0 +1,3 @@
+module github.com/ingcapadev/https-urls
+
+go 1.23.0
---
+++ http/2-urls/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+const itemURL = "https://api.boot.dev/v1/courses_rest_api/learn-http/items"
+
+func main() {
+	items, err := getItemData(itemURL)
+	if err != nil {
+		log.Fatalf("error getting item data: %v", err)
+	}
+	prettyData, err := prettify(string(items))
+	if err != nil {
+		log.Fatalf("error prettifying data: %v", err)
+	}
+	fmt.Println(prettyData)
+}
