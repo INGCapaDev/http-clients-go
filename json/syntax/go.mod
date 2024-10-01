067504f8a57a91c1b44048824fd77da4c6c75fcd
INGCapaDev <ingcapadev@gmail.com>
2024-10-01 13:59:35 -0700

    :sparkles: [FEAT](json): JSON syntax in go

---
+++ json/syntax/go.mod
@@ -0,0 +1,3 @@
+module github.com/ingcapadev/json-syntax
+
+go 1.23.0
---
+++ json/syntax/main.go
@@ -0,0 +1,27 @@
+package main
+
+const itemList = `
+[
+		{
+			"id": 0,
+			"name": "sword",
+			"damage": 10.5,
+			"equipped": false
+		},
+		{
+			"id": 1,
+			"name": "shield",
+			"damage": 5.5,
+			"equipped": true
+		}
+]
+`
+
+const playerObject = `
+{
+	"name": "Fudd",
+	"items": "spear and magic helmet",
+	"wife": "Brunhilde",
+	"power": 9000
+}
+`
